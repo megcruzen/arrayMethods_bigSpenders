d00e90aca05cbfc5ab9c5c026e42c8106aa04860
Meg Cruzen <megcruzen@gmail.com>
2019-01-09 21:18:18 -0600

    Exercise complete

--- main.js
+++ main.js
@@ -8,5 +8,23 @@ Use the filter method to get all the big spenders in the main array into a new o
 const bigSpenders = businesses.filter(business => {
 
 })
+*/
 
+const container = document.querySelector("#container")
+container.innerHTML = "<h1>Big Spenders</h1>"
+
+const bigSpenders = businesses.filter(business =>  business.orders.some(x => x > 9000));
+
+console.log(bigSpenders);
+
+bigSpenders.forEach(business => {
+    container.innerHTML += `
+      <div>${business.companyName}</div>
+    `
+  });
+
+
+/*
+[2, 5, 8, 1, 4].some(x => x > 10);  // false
+[12, 5, 8, 1, 4].some(x => x > 10); // true
 */
